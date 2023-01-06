6ce26d4ea95a537e0cf97b7823fac954c3bd716f
zixiao-h <zixiao.hu.09@gmail.com>
2023-01-06 23:18:40 +0000

    Added script to generate pickled test samples

---
+++ generate_test_samples.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+from toy_samples import gaussian_samples, wedding_cake_samples, correlated_gaussian_samples
