63f7fcaea41909567c4ed8a58506c51933585c07
zixiao-h <zixiao.hu.09@gmail.com>
2023-02-16 16:58:57 +0000

    added analytic X statistics and predictions

---
+++ notebooks/week_4/true_distribution.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def logt_sample(n):
+    """Generate logt for given number of live points n"""
+    p = np.random.rand()
+    return 1/n * np.log(p)
+logt_sample = np.vectorize(logt_sample)
+
+def X_sample(nk):
+    """Generate single sample of X at the end of a live point distribution nk"""
+    logt_samples = logt_sample(nk)
+    logX = np.sum(logt_samples)
+    X = np.exp(logX)
+    return X
+
+def generate_Xs(nk, iterations=None):
+    """Generates the Xs at each iteration in a run with live point distribution nk"""
+    logt_samples = logt_sample(nk)
+    logXs = np.cumsum(logt_samples)
+    Xs = np.exp(logXs)
+    if iterations is not None:
+        return np.take(Xs, iterations)
+    return Xs
+
+def generate_Xsamples(nk, n_samples=1000, iterations=None):
+    """Repeats generate_Xs n_samples times and returns result as an array <n_samples> long"""
+    if iterations is None:
+        n_iterations = len(nk)
+    else:
+        n_iterations = len(iterations)
+    X_samples = np.zeros((n_samples, n_iterations))
+    for i in range(n_samples):
+        X_samples[i] = generate_Xs(nk, iterations)    
+    return X_samples
