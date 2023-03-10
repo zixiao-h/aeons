d6ddc990df5db50c3712c95b7867b94737264eb9
zixiao-h <zixiao.hu.09@gmail.com>
2023-03-10 16:41:22 +0000

    refactoring models as classes for ease of use

--- aeons/likelihoods.py
+++ aeons/likelihoods.py
@@ -12,10 +12,12 @@ def linear_like():
     def func(X, theta):
         return theta * X
 
-    def inverse(y, theta):
+    def inverse(y, theta, torch=True):
         return y / theta
 
-    def prime(X, theta):
+    def prime(X, theta, torch=False):
+        if torch:
+            return theta * torch.ones_like(X)
         return theta * np.ones_like(X)
 
     return likelihood(func, inverse, prime)
@@ -25,7 +27,7 @@ def quad_like():
     def func(X, theta):
         return theta * X**2
 
-    def inverse(y, theta):
+    def inverse(y, theta, torch=True):
         return (y/theta)**(1/2)
 
     def prime(X, theta):
@@ -38,7 +40,9 @@ def log_like():
     def func(X, theta):
         return theta * np.log(X)
 
-    def inverse(y, theta):
+    def inverse(y, theta, torch=False):
+        if torch:
+            return torch.exp(y/theta)
         return np.exp(y/theta)
 
     def prime(X, theta):
@@ -47,67 +51,46 @@ def log_like():
     return likelihood(func, inverse, prime)
 
 
-def simple_like(logX=False):
+def simple_like():
     def func(X, d):
-        if logX:
-            X = np.exp(X)
         return -X**(2/d)
 
-    def inverse(logL, d):
-        if logX:
-            return np.log((-logL)**(d/2))
+    def inverse(logL, d, torch=True):
         return (-logL)**(d/2)
 
     def prime(X, d):
-        if logX:
-            X = np.exp(X)
-            return - (2/d) * X**(2/d)
         return - (2/d) * X**(2/d - 1)
 
     return likelihood(func, inverse, prime)
 
 
-def middle_like(logX=False):
+def middle_like():
     def func(X, theta):
         d, sigma = theta
-        if logX:
-            X = np.exp(X)
         return - X**(2/d)/(2*sigma**2)
 
-    def inverse(logL, theta):
+    def inverse(logL, theta, torch=True):
         d, sigma = theta
-        if logX:
-            return np.log((-2*sigma**2*logL)**(d/2))
         return (-2*sigma**2*logL)**(d/2)
 
     def prime(X, theta):
         d, sigma = theta
-        if logX:
-            X = np.exp(X)
-            return - (1/d*sigma**2) * X**(2/d)
         return - (1/d*sigma**2) * X**(2/d - 1)
     
     return likelihood(func, inverse, prime)
 
 
-def full_like(logX=False):
+def full_like():
     def func(X, theta):
         logLmax, d, sigma = theta
-        if logX:
-            X = np.exp(X)
         return logLmax - X**(2/d)/(2*sigma**2)
 
-    def inverse(logL, theta):
+    def inverse(logL, theta, torch=True):
         logLmax, d, sigma = theta
-        if logX:
-            return np.log((2*sigma**2 * (logLmax - logL))**(d/2))
         return (2*sigma**2 * (logLmax - logL))**(d/2)
 
     def prime(X, theta):
         logLmax, d, sigma = theta
-        if logX:
-            X = np.exp(X)
-            return - 1/(d*sigma**2) * X**(2/d)
         return - 1/(d*sigma**2) * X**(2/d - 1)
     
     return likelihood(func, inverse, prime)
