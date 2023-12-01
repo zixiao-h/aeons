3ca9763ee333e2c84fb01ea09066d93b60612ad4
zixiao-h <zixiao.hu.09@gmail.com>
2023-12-01 03:36:28 +0000

    updated figures to render properly

--- paper/figures/dead_measure.py
+++ paper/figures/dead_measure.py
@@ -21,9 +21,21 @@ for i, ax in enumerate(axes):
     ax.set_xticks([])
     ax.set_yticks([])
     ax.set_box_aspect(1)
-    points = samples.iloc[ndeads[i] - prev[i] : ndeads[i]]
-    ax.scatter(points[x], points[y], s=0.1)
-    # ax.scatter(samples[x], samples[y], s=0.1)
+    points = samples.iloc[: ndeads[i] : 2]
+    if i == 0:
+        lives = samples.live_points(1)
+    else:
+        lives = samples.live_points(ndeads[i - 1])
+    deads = points[(points[x] < lives[x].max()) * (points[y] < lives[y].max()) * (points[x] > lives[x].min()) * (points[y] > lives[y].min())]
+    data = np.array([deads[x].values, deads[y].values]).T
+    print(len(data))
+    z0 = ax.get_zorder()
+    zorders = z0 + 1 - 0.0001 * np.arange(len(data))
+    for j, point in enumerate(data):
+        # ax.plot(*point, lw=.2, color='C0', zorder=zorders[j])
+        ax.plot(*point, 'o', ms=.4, color='C0', zorder=zorders[j])
+    ax.set_rasterization_zorder(z0 + 1 - 0.0001 * 0.8 * len(data))
+    print(i)
 
 axes[0].get_children()[-1].get_zorder()
 axes[0].set_zorder(10)
@@ -88,7 +100,7 @@ ax = fig.add_subplot(1,4,4)
 ax.set_box_aspect(1)
 ax.set_xticks([])
 ax.set_yticks([])
-ax.scatter(samples[x], samples[y], s=0.2)
+ax.plot(samples.iloc[7000::2][x], samples.iloc[7000::2][y], 'o', ms=0.4)
 axes = np.concatenate([axes, [ax]])
 
 fig.canvas.draw()
@@ -97,7 +109,7 @@ plot_live_points = True
 
 for k, (i, ax0, ax1) in enumerate(zip(ndeads, axes[:-1], axes[1:])):
     if plot_live_points:    
-        live = samples.live_points(i)
+        live = samples.live_points(i).iloc[::2]
         ax0.scatter(live[x], live[y], s=2, color=f'C{k+1}')
         ax1.scatter(live[x], live[y], s=2, color=f'C{k+1}')
     draw_zoom_lines(ax0, ax1, *get_box(i))
@@ -110,4 +122,4 @@ if plot_live_points:
 else:
     filename = 'dead_measure.pdf'
 
-fig.savefig(filename, transparent=False, bbox_inches='tight')
+fig.savefig(filename, bbox_inches='tight')
