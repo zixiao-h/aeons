8db5c18cd8b42de04d27aeec860c89529f2388d2
zixiao-h <zixiao.hu.09@gmail.com>
2024-09-06 15:28:52 +0100

    new test file

--- aeons/plotting.py
+++ aeons/plotting.py
@@ -1,8 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from aeons.utils import *
-from aeons.regress import GaussianRegress, analytic_lm_params, params_from_d
-from aeons.likelihoods import full
 
 from anesthetic.utils import quantile
 from anesthetic.plot import quantile_plot_interval
@@ -67,49 +65,5 @@ def plot_std(xvals, y_means, y_stds, true=None, ylim=None, ax=None, color='deeps
             ax.set_ylim(ylim[0]*true, ylim[1]*true)
 
     
-def plot_split(model, ndead, nlive, splits=2, trunc=None):
-    logLlive, Xlive, nk, logZdead = model.data(ndead, live=True)
-    regress = GaussianRegress(logLlive, Xlive)
-    logXf = logXf_formula(regress.theta, logZdead, Xlive[0])
-    endpoint = calc_endpoints(ndead, np.log(Xlive[0]), logXf, 0.01, nlive=nlive)[0]
 
-    logL_split, X_split = data_split(logLlive, Xlive, splits, trunc)
-    regress_split = GaussianRegress(logL_split, X_split)
-    logXf_split = logXf_formula(regress_split.theta, logZdead, Xlive[0])
-    endpoint_split = calc_endpoints(ndead, np.log(Xlive[0]), logXf_split, 0.01, nlive=nlive)[0]
-
-    fig, axs = plt.subplots(1, 2, figsize=(8, 3))
-    axs[0].plot(Xlive, logLlive, '+', ms=3)
-    axs[0].plot(Xlive, full.func(Xlive, regress.theta), lw=2, label=f"{endpoint:.0f}")
-    axs[0].set_title(f"{formatt(regress.theta)} logZ = {regress.logZ():.1f}")
-    axs[0].legend()
-    axs[1].plot(Xlive, logLlive, '+', ms=3)
-    axs[1].plot(X_split, full.func(X_split, regress_split.theta), lw=2, label=f"{endpoint_split:.0f}")
-    axs[1].set_title(f"{formatt(regress_split.theta)} logZ = {regress_split.logZ():.1f}")
-    axs[1].set_yticks([])
-    axs[1].legend()
-    fig.suptitle(f"ndead = {ndead}, true = {model.true_endpoint():.0f}, {model.true_logXf():.1f}", y=0.95)
-    fig.tight_layout()
-    
-def plot_fit(model, ndead, d=None, ax=None, **kwargs):
-    points = model.points(ndead)
-    logL, X_mean, nk, logZdead = data(points)
-    logLd, Xd = logL[ndead:], X_mean[ndead:]
-    if ax is None:
-        fig, ax = plt.subplots()
-    ax.plot(Xd, logLd, 'x', ms=1, color='black', label=f"True $-\\log X_\\mathrm{{f}}={-model.true_logXf():.1f}$")
-    
-    color = kwargs.pop('color', 'dodgerblue')
-    lw = kwargs.pop('lw', 2)
-    if d is None:
-        theta = analytic_lm_params(logLd, Xd, 1)
-        logXf = logXf_formula(theta, logZdead, X_mean[ndead])
-        label = f"$d={theta[1]:.2f}$, $-\\log \\hat{{X}}_\\mathrm{{f}}={-logXf:.1f}$"
-        ax.plot(Xd, full.func(Xd, theta), color=color, lw=lw, label=label, **kwargs)
-    else:
-        theta = params_from_d(logLd, Xd, d)
-        logXf = logXf_formula(theta, logZdead, X_mean[ndead])
-        label = f"$d={d:.2f}$, $-\\log \\hat{{X}}_\\mathrm{{f}}={-logXf:.1f}$"
-        ax.plot(Xd, full.func(Xd, theta), color=color, lw=lw, label=label, **kwargs)
-    ax.legend()
  
